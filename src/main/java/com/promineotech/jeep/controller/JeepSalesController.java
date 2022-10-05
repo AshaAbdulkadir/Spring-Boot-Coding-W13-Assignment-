d2f91de26f422f5f4637a2c4f1060202be7fc1b5
AshaAbdu <halge87@yahoo.com>
2022-10-04 20:07:35 -0500

    Rest controller annotation and Rest API

--- src/main/java/com/promineotech/jeep/controller/JeepSalesController.java
+++ src/main/java/com/promineotech/jeep/controller/JeepSalesController.java
@@ -1,7 +1,14 @@
 package com.promineotech.jeep.controller;
 
 import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import com.promineotech.jeep.entity.Jeep;
+import com.promineotech.jeep.entity.JeepModel;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -11,6 +18,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.servers.Server;
 
+@RequestMapping("/jeeps")
 @OpenAPIDefinition(info = @Info(title = "Jeep Sales Service"), servers = {
 		@Server(url = "http://localhost:8080", description = "Local server.")})
 public interface JeepSalesController {
@@ -51,8 +59,11 @@ public interface JeepSalesController {
 							description = "The trim level (i.e 'sport')")
 			}
 	)
-	// @formatter:on
 	
-	List<Jeep> fetchJeeps(String model, String trim);
-
+	@GetMapping
+	@ResponseStatus(code = HttpStatus.OK)
+	  List<Jeep> fetchJeeps(
+			  @RequestParam JeepModel model, 
+			  @RequestParam String trim);
+// @formatter:on
 }
