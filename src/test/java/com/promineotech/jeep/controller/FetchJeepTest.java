b84669d8ff999e02a07277478256d87111788d88
AshaAbdu <halge87@yahoo.com>
2022-10-04 20:29:41 -0500

    implementation

--- src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
+++ src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
@@ -1,6 +1,9 @@
 package com.promineotech.jeep.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +11,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
@@ -36,10 +43,10 @@ class FetchJeepTest {
 		System.out.println(uri);
 		
 		// When: a connection is made to the URI
-		getRestTemplate().getForEntity(uri, Jeep.class);
+		ResponseEntity<List<Jeep>> response = restTemplate.exchange(uri, HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
 		
 		// Then: a success (OK - 200) status code is returned
-		
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
 
