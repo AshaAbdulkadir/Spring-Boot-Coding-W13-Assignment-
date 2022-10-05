8b7a1dbd56b44117a480a15febd45444e43d3a98
AshaAbdu <halge87@yahoo.com>
2022-10-04 19:40:01 -0500

    Parameter annotations

--- src/main/java/com/promineotech/jeep/controller/JeepSalesController.java
+++ src/main/java/com/promineotech/jeep/controller/JeepSalesController.java
@@ -4,7 +4,10 @@ import java.util.List;
 import com.promineotech.jeep.entity.Jeep;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.servers.Server;
 
@@ -17,13 +20,35 @@ public interface JeepSalesController {
 			summary = "Returns a list of jeeps",
 			description = "Returnsa list of Jeeps given an optional model and /or trim",
 			responses = {
-					@ApiResponse(responseCode = "200", description = "A list of jeeps is returned"),
-					@ApiResponse(responseCode = "400", description = "The request parameters are invalid"),
-					@ApiResponse(responseCode = "404", description = "No jeeos were found with input criteria"),
-					@ApiResponse(responseCode = "500", description = "An unplanned error occured.")
+					@ApiResponse(
+							responseCode = "200", 
+							description = "A list of jeeps is returned", 
+							content = @Content(mediaType = "application/json", 
+							schema = @Schema(implementation = Jeep.class))),
+					@ApiResponse(
+							responseCode = "400", 
+							description = "The request parameters are invalid", 
+							content = @Content(mediaType = "application/json")),
+					@ApiResponse(
+							responseCode = "404", 
+							description = "No jeeos were found with input criteria", 
+							content = @Content(mediaType = "application/json")),
+					@ApiResponse(
+							responseCode = "500", 
+							description = "An unplanned error occured.", 
+							content = @Content(mediaType = "application/json"))
 			},
 			parameters = {
-					
+					@Parameter(
+							name = "model", 
+							allowEmptyValue = false, 
+							required = false, 
+							description = "The model name (i.e 'WRANGLER')"),
+					@Parameter(
+							name = "trim", 
+							allowEmptyValue = false, 
+							required = false, 
+							description = "The trim level (i.e 'sport')")
 			}
 	)
 	// @formatter:on
