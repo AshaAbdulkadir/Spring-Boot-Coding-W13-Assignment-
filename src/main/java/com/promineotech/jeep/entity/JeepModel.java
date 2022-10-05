8f895198ed936bf4889139a2510f8bdabd23ece4
AshaAbdu <halge87@yahoo.com>
2022-10-04 19:08:58 -0500

    Open API documentation

---
+++ src/main/java/com/promineotech/jeep/entity/JeepModel.java
@@ -0,0 +1,6 @@
+package com.promineotech.jeep.entity;
+
+public enum JeepModel {
+	WRANGLER, GRAND_CHEROKEE, CAMPASS, RENEGADE, GLADIATOR, WRANGLER_4XE
+
+}
--- src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
+++ src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
@@ -3,11 +3,19 @@ package com.promineotech.jeep.controller;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
+
+import com.promineotech.jeep.entity.Jeep;
+import com.promineotech.jeep.entity.JeepModel;
+
+import lombok.Getter;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
@@ -18,8 +26,28 @@ import org.springframework.test.context.jdbc.SqlConfig;
 class FetchJeepTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testThatJeepsAreReturnedWhenAValidModelAndTrimAreSupplied() {
+		// Given a valid model, trim and URI
+		JeepModel model = JeepModel.WRANGLER;
+		String trim = "Sport";
+		String uri = 
+				String.format("http://localhost:%d/jeeps?model=%s&trim=%s", serverPort, model, trim);
+		
+		System.out.println(uri);
+		
+		// When: a connection is made to the URI
+		getRestTemplate().getForEntity(uri, Jeep.class);
+		
+		// Then: a success (OK - 200) status code is returned
+		
 	}
 
+
+	@Autowired
+	@Getter
+	private TestRestTemplate restTemplate;
+	
+	@LocalServerPort
+	private int serverPort;
+	
 }
